620ec7bdd94388cec9df9ec7bae219129a62925f
Kira <kira.eva@protonmail.com>
2024-10-11 10:41:04 +0100

    actionbar improvements
    fix actionbar ui scaling
    
    add hotkeys

---
+++ code/City/CityAction.cs
@@ -0,0 +1,25 @@
+﻿namespace Kira;
+
+using System;
+
+public class CityAction
+{
+    public float Cooldown { get; set; }
+    public TimeSince TimeSinceLastUse { get; set; }
+    public Action OnUseAction { get; set; }
+
+    public CityAction(Action onUseAction, float coolDown = 1f)
+    {
+        this.OnUseAction = onUseAction;
+        this.Cooldown = coolDown;
+    }
+
+    public void Use()
+    {
+        if (!CanUse) return;
+        TimeSinceLastUse = 0;
+        OnUseAction?.Invoke();
+    }
+
+    public bool CanUse => TimeSinceLastUse > Cooldown;
+}
